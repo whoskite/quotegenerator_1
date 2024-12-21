341d1590040413aa1b521ae10b74b644d145606e
KITE <whoskitemedia@gmail.com>
2024-12-20 16:48:19 -0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const Demo = dynamic(() => import('./components/Demo'), {
+  ssr: false,
+});
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col p-4">
+      <Demo />
+    </main>
+  );
+}
